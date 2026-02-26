5e5665508de55e4ffc5051d0be31308571efdad3
Nguyễn Tuấn Thanh <ntt.thanh.2005@gmail.com>
2026-02-26 08:39:23 +0700

    Create SayHello.java
    bai code dau tien

---
+++ Bai1_Hello/src/SayHello.java
@@ -0,0 +1,13 @@
+public class SayHello {  
+	
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		System.out.println("Xin chao. Toi la sinh vien khoa k66 CNTT");
+		System.out.println("Nguyen Tuan Thanh");
+		System.out.println("5 + 3 = " + (5 + 3));
+		
+	}
+
+}
