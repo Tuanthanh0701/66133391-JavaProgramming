0d4584a10d78fcb1094d58eb4fe37ddb029ed7b2
Nguyễn Tuấn Thanh <ntt.thanh.2005@gmail.com>
2026-03-01 20:15:14 +0700

    Create tinhtonghaiso.java
    up

---
+++ Bai3_TH1/src/tinhtonghaiso.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class tinhtonghaiso {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhập số thứ nhất: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Nhập số thứ hai: ");
+        double num2 = scanner.nextDouble();
+        
+        double sum = num1 + num2;
+        System.out.printf("Tổng của %.2f và %.2f là: %.2f");
+        
+        scanner.close();
+    }
+}
