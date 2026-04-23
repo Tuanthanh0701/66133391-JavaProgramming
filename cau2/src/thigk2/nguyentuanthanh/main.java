67ac624ddcca3cf1988ba311fb732909ff907eac
Nguyễn Tuấn Thanh <ntt.thanh.2005@gmail.com>
2026-04-23 07:48:18 +0700

    push lan 1

---
+++ cau2/src/thigk2/nguyentuanthanh/main.java
@@ -0,0 +1,58 @@
+package thigk2.nguyentuanthanh;
+
+public class main {
+	public class SinhVien {
+	    private String maSV;
+	    private String hoTen;
+	    private int namSinh;
+	    private String chuyenNganh; // Thuộc tính tự thêm thứ 4
+
+	    // Constructor đầy đủ tham số
+	    public SinhVien(String maSV, String hoTen, int namSinh, String chuyenNganh) {
+	        this.maSV = maSV;
+	        this.hoTen = hoTen;
+	        this.namSinh = namSinh;
+	        this.chuyenNganh = chuyenNganh;
+	    }
+
+	    // Các Getter và Setter
+	    public String getMaSV() {
+	        return maSV;
+	    }
+
+	    public void setMaSV(String maSV) {
+	        this.maSV = maSV;
+	    }
+
+	    public String getHoTen() {
+	        return hoTen;
+	    }
+
+	    public void setHoTen(String hoTen) {
+	        this.hoTen = hoTen;
+	    }
+
+	    public int getNamSinh() {
+	        return namSinh;
+	    }
+
+	    public void setNamSinh(int namSinh) {
+	        this.namSinh = namSinh;
+	    }
+
+	    public String getChuyenNganh() {
+	        return chuyenNganh;
+	    }
+
+	    public void setChuyenNganh(String chuyenNganh) {
+	        this.chuyenNganh = chuyenNganh;
+	    }
+
+	    // Ghi đè phương thức toString để in thông tin sinh viên cho đẹp
+	    @Override
+	    public String toString() {
+	        return "Mã SV: " + maSV + " | Họ tên: " + hoTen + 
+	               " | Năm sinh: " + namSinh + " | Chuyên ngành: " + chuyenNganh;
+	    }
+	}
+}
